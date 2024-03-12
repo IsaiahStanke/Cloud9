e1d9f0343a265397373c5278f5a026282366ed46
IsaiahStanke <83856624+IsaiahStanke@users.noreply.github.com>
2024-03-12 15:41:07 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='Cloud9',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/IsaiahStanke/Cloud9',
+    license='GNU General Public License v3.0',
+    author='Isaiah Stanke',
+    author_email='',
+    description='Signature-based Instrusion Detection System',
+    install_requires=['scapy']
+)
